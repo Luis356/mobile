42a310fef8b2718e1b264046bbb390485d6b5ad8
EduardoDfs <odraudesaiddfs@gmail.com>
2022-09-30 09:49:43 -0300

    dev mobile

---
+++ dev_mobile/bin/dev_mobile.dart
@@ -0,0 +1,11 @@
+void main() {
+
+  dynamic vX = 100;
+  print(vX);
+  print(vX.runtimeType);
+
+  vX = "Vinicio cornão";
+  print(vX);
+  print(vX.runtimeType);
+
+}
---
+++ dev_mobile/test/dev_mobile_test.dart
@@ -0,0 +1,8 @@
+import 'package:dev_mobile/dev_mobile.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
